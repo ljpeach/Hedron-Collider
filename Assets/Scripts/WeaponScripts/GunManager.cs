d410af28c2b5e80856cb2ff83d8c7e2cc19b8a21
Liam Peachey <liamjpeachey@gmail.com>
2020-11-03 18:41:16 -0500

    Gun Animation
    Added recoil animation for gun.

---
+++ Assets/Scripts/WeaponScripts/GunManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager: MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !anim.isPlaying)
+        {
+            anim.Play("Recoil");
+        }
+    }
+}
