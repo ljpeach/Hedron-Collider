171d6ef108c7d0d224092a2ba4c74c28ecc99b9f
Liam Peachey <liamjpeachey@gmail.com>
2020-11-03 21:55:59 -0500

    Weapon Switching
    Weapon switching!

---
+++ Assets/Scripts/WeaponScripts/WeaponSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    public GameObject sword;
+    public GameObject gun;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            sword.SetActive(false);
+            gun.SetActive(true);
+        }
+        else if (Input.GetButtonUp("Fire2"))
+        {
+            sword.SetActive(true);
+            gun.SetActive(false);
+        }
+    }
+}
