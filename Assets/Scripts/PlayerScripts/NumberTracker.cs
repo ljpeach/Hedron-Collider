c31dbda97df4d60e5bcd2ef1d2ff4454083026e8
Liam Peachey <liamjpeachey@gmail.com>
2020-11-04 03:59:30 -0500

    Implemented Health Regen

--- Assets/Scripts/PlayerScripts/NumberTracker.cs
+++ Assets/Scripts/PlayerScripts/NumberTracker.cs
@@ -10,21 +10,43 @@ public class NumberTracker : MonoBehaviour
     public int healthMax;
     public TextMeshProUGUI healthBar;
     public TextMeshProUGUI campBar;
+    public bool regen;
+    public float regenRate;
+    public float regenDelay;
 
-    int currentHealth;
+    float currentHealth;
     void Start()
     {
         currentHealth = healthMax;
         setCountText();
+        regen = true;
+    }
+
+    void Update()
+    {
+        if (regen && currentHealth < healthMax)
+        {
+            currentHealth += regenRate * Time.deltaTime;
+            setCountText();
+
+        }
+        else if (regen)
+        {
+            currentHealth = healthMax;
+            setCountText();
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("playerDamage"))
         {
+            CancelInvoke();
             currentHealth -= other.gameObject.GetComponent<DealDamage>().damage;
             setCountText();
             other.gameObject.tag = "Untagged";
+            regen = false;
+            Invoke("regenOn", regenDelay);
         }
         if (currentHealth <= 0)
         {
@@ -36,6 +58,11 @@ public class NumberTracker : MonoBehaviour
     {
         healthBar.text = "Health: " + currentHealth.ToString() + "/" + (healthMax).ToString();
         campBar.text = "Camps:" + campCount.ToString();
+    }
+
+    void regenOn()
+    {
+        regen = true;
     }
 
     void destroySequence()
