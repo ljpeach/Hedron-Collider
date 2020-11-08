6de96db42023e4c50ea8cb158fc5ffd0b340f2be
Liam Peachey <liamjpeachey@gmail.com>
2020-11-08 12:57:25 -0500

    Ranged Movement
    Established ranged enemy movement pattern

--- Assets/Scripts/EnemyScripts/Ranged/MainRanged.cs
+++ Assets/Scripts/EnemyScripts/Ranged/MainRanged.cs
@@ -6,18 +6,38 @@ public class MainRanged : MonoBehaviour
 {
     public GameObject player;
     public float RoF;
+    public float radius = 5;
+    public float turnSpeed;
+    public float healthMax;
+    public float currentHealth;
+    public int direction;
+
+    float speed;
+    float angle = 0;
     float timer;
     ProjectileValues gun;
+    Vector3 center;
+    float height;
 
     void Start()
     {
+        direction = 1;
+        speed = (2 * Mathf.PI) / turnSpeed;
         timer = 0;
         gun = GetComponentInChildren<ProjectileValues>();
+        center = player.transform.position;
+        height = transform.position.y;
+        currentHealth = healthMax;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Mathf.Pow(player.transform.position.x - center.x, 2) + Mathf.Pow(player.transform.position.z - center.z, 2) > radius*radius)
+        {
+            direction *= -1;
+            updateCenter();
+        }
         transform.LookAt(player.transform.position);
         if (timer >= RoF)
         {
@@ -25,5 +45,18 @@ public class MainRanged : MonoBehaviour
             timer = 0;
         }
         timer += Time.deltaTime;
+        angle += speed * Time.deltaTime *direction;
+        transform.position = new Vector3(Mathf.Cos(angle) * radius + center.x, height, Mathf.Sin(angle) * radius + center.z);
+        
+    }
+
+    public void destroySequence()
+    {
+        Destroy(gameObject);
+    }
+
+    void updateCenter()
+    {
+        center = player.GetComponent<Transform>().position;
     }
 }
