604fa586061e46f26801626431db1075f6c9e099
Liam Peachey <liamjpeachey@gmail.com>
2020-11-08 07:23:51 -0500

    Added Camp pickup Behavior
    Added camp pickup, and it bobs up down while rotating!

---
+++ Assets/Scripts/Camps/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    float startHeight;
+    float x;
+    void Start()
+    {
+        startHeight = transform.position.y;
+        x = 0;
+    }
+    void Update()
+    {
+        transform.Translate( new Vector3(0f, Mathf.Sin(x), 0f)*Time.deltaTime*.2f);
+        x += Time.deltaTime;
+        if (x >= Mathf.PI * 2)
+        {
+            x = 0;
+        }
+        transform.Rotate(new Vector3(0, 90f, 0) * Time.deltaTime);
+    }
+}
