cc47002ca502d6bea2d3c59db28f7f367efd1108
Liam Peachey <liamjpeachey@gmail.com>
2020-11-08 11:22:22 -0500

    Ranged Enemy Start
    Got layout for enemy, enemy shoots occasionally.

---
+++ Assets/Scripts/EnemyScripts/Ranged/MainRanged.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRanged : MonoBehaviour
+{
+    public GameObject player;
+    public float RoF;
+    float timer;
+    ProjectileValues gun;
+
+    void Start()
+    {
+        timer = 0;
+        gun = GetComponentInChildren<ProjectileValues>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+        if (timer >= RoF)
+        {
+            gun.Shoot();
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
